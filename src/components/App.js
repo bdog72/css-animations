aa1cbca2181281195fdf5e8b241421955ebfd3d0
Brian Bycynski <high5bri@hotmail.com>
2017-10-07 07:25:02 -0400

    css animation practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-class App extends Component {
+export default class App extends Component {
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      {/* <h1 className='whatUpDog'>What up dog</h1>
+      // <p className='box'>Box</p> */}
+      {/* <div className='sun' />
+      <div className='grass' /> */}
+      <div className='container'>
+        <h1>This Page Takes Forever To Load</h1>
+        <div className='loader' />
+        <h2>It never loads, you silly goose</h2>
+      </div>
     </div>
   }
 }
-
-export default App
